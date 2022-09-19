619b89485904864f2aa784c86de9264a39b97d6f
Nguyen Nhan Tinh <nguyennhantinh@outlook.com>
2022-09-19 10:50:45 +0900

    add PC app

---
+++ Pulse_setting/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Pulse_setting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_snd = new System.Windows.Forms.Button();
+            this.btn_com = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(171, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(145, 27);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM Port";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(171, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "16000";
+            // 
+            // btn_snd
+            // 
+            this.btn_snd.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_snd.Location = new System.Drawing.Point(380, 86);
+            this.btn_snd.Name = "btn_snd";
+            this.btn_snd.Size = new System.Drawing.Size(120, 28);
+            this.btn_snd.TabIndex = 3;
+            this.btn_snd.Text = "send";
+            this.btn_snd.UseVisualStyleBackColor = true;
+            this.btn_snd.Click += new System.EventHandler(this.btn_snd_Click);
+            // 
+            // btn_com
+            // 
+            this.btn_com.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_com.Location = new System.Drawing.Point(380, 23);
+            this.btn_com.Name = "btn_com";
+            this.btn_com.Size = new System.Drawing.Size(120, 30);
+            this.btn_com.TabIndex = 3;
+            this.btn_com.Text = "Open";
+            this.btn_com.UseVisualStyleBackColor = true;
+            this.btn_com.Click += new System.EventHandler(this.btn_com_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(322, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 19);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "kHz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(12, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pulse Frequency";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 175);
+            this.Controls.Add(this.btn_com);
+            this.Controls.Add(this.btn_snd);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_snd;
+        private System.Windows.Forms.Button btn_com;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Pulse_setting/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Management;
+
+namespace Pulse_setting
+{
+    public partial class Form1 : Form
+    {
+        private int _isClose = 0;
+        private SerialPort _comPort;
+        public Form1()
+        {
+            InitializeComponent();
+            using (var searcher = new ManagementObjectSearcher
+                ("SELECT * FROM WIN32_SerialPort"))
+            {
+                string[] portnames = SerialPort.GetPortNames();
+                foreach (string port in portnames)
+                {
+                    comboBox1.Items.Add(port);
+                }
+                comboBox1.Text = comboBox1.Items[0].ToString();
+                var _ports = searcher.Get().Cast<ManagementBaseObject>().ToList();
+                var tList = (from n in portnames
+                             join p in _ports on n equals p["DeviceID"].ToString()
+                             select n + " - " + p["Caption"]).ToList();
+                int cnt = 0;
+                foreach(var t in tList)
+                {
+                    if (t.Contains("STLink"))
+                    {
+                        comboBox1.Text = comboBox1.Items[cnt].ToString();
+                        break;
+                    }
+                    cnt++;
+                }
+            }
+            
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            btn_snd.Enabled = false;
+        }
+
+        private void btn_snd_Click(object sender, EventArgs e)
+        {
+            UInt32 frq = UInt32.Parse(textBox1.Text);
+            byte[] data = new byte[4];
+            data[0] = (byte)((frq >> 24) & 0xFF);
+            data[1] = (byte)((frq >> 16) & 0xFF);
+            data[2] = (byte)((frq >> 08) & 0xFF);
+            data[3] = (byte)((frq >> 00) & 0xFF);
+            _comPort.Write(data, 0, 4);
+        }
+
+        private void btn_com_Click(object sender, EventArgs e)
+        {
+            if(_isClose == 0)
+            {
+                // Open COM port
+                _comPort = new SerialPort();
+                _comPort.PortName = comboBox1.Text;
+                _comPort.BaudRate = 115200;
+                _comPort.DataBits = 8;
+                _comPort.Parity = Parity.None;
+                _comPort.Handshake = Handshake.None;
+                _comPort.ReadTimeout = 500;
+                _comPort.WriteTimeout = 500;
+
+                _comPort.Open();
+                _isClose = 1;
+                btn_com.Text = "Close";
+                btn_snd.Enabled = true;
+            }
+            else
+            {
+                // Close Port
+                _comPort.Close();
+                _isClose = 0;
+                btn_com.Text = "Open";
+                btn_snd.Enabled = false;
+            }
+        }
+    }
+}
